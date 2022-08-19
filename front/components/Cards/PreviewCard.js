edc222a514c8228b27340e41c3c80c42fe0938a2
aldrn <aldrn29@gmail.com>
2022-08-20 01:04:02 +0900

    feat: 카드 액션 추가

--- front/components/Cards/PreviewCard.js
+++ front/components/Cards/PreviewCard.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { styled as materialStyled } from "@mui/material/styles";
-import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 
@@ -10,19 +8,20 @@ const PreviewCard = () => {
     return (
         <CardWrapper>
             <CardDate>2022. 8. 17.</CardDate>
-            <CardMedia
-                component="img"
-                height="100"
-                image="/background.jpg"
-                alt="green iguana"
-            />
-            <CardInfo>
-                <Title>
-                    망원동 카페 - 티노마드
-                </Title>
-                <SubTitle>안녕하세요. 오랜만에 카페 업로드 글로 찾아왔습니다. 이번에 가본 카페는 망원동에 있는 티노마드라는 카페인데요.</SubTitle>
-            </CardInfo>
-            *
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="100"
+                    image="/background.jpg"
+                    alt="green iguana"
+                />
+                <CardInfo>
+                    <Title>
+                        망원동 카페 - 티노마드
+                    </Title>
+                    <SubTitle>안녕하세요. 오랜만에 카페 업로드 글로 찾아왔습니다. 이번에 가본 카페는 망원동에 있는 티노마드라는 카페인데요.</SubTitle>
+                </CardInfo>
+            </CardActionArea>
             <Line />
         </CardWrapper>
     )
