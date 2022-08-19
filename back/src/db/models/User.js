da349650cdfbd2c531baf9144e047c05dd1a2f46
guswn2521@gmail.com <guswn2521@gmail.com>
2022-08-19 18:41:33 +0900

    sequelize query 시도

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -1,11 +1,17 @@
 import db from "..";
 const userModel = db.user;
+const followModel = db.follow;
 const Sequelize = db.Sequelize;
 const sequelize = db.sequelize;
 
 const User = {
   findAll: async () => {
-    const users = await userModel.findAll();
+    const follow_users = await followModel.
+    const users = await userModel.findAll({
+      where: {
+        grade:'컬리언서'
+      }
+    });
     return users;
   },
 };
