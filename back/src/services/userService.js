e9e5cee2544246e0dadda463937ef7c43eafe89e
guswn2521@gmail.com <guswn2521@gmail.com>
2022-08-20 11:08:29 +0900

    feat:닉네임 login,

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,5 +1,6 @@
 import { User } from "../db/models/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 class UserService {
   static async getUsers() {
@@ -26,11 +27,28 @@ class UserService {
       const errorMessage = "해당 유저가 없습니다.";
       throw new Error(errorMessage);
     }
-    const isPasswordCorrect = await bcrypt.compare(user.password, password);
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
     if (!isPasswordCorrect) {
       const errorMessage = "잘못입력하셨습니다.";
       throw new Error(errorMessage);
     }
+
+    // last login update 추가
+
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const token = jwt.sign({ userId: user.user_id }, secretKey);
+
+    const { user_id, picture, grade, intro } = user;
+    const loginUser = {
+      token,
+      userId: user_id,
+      nickname,
+      picture,
+      grade,
+      intro,
+    };
+    return { message: "success", data: loginUser };
   }
 }
 
