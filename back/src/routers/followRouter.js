988cec74f57a29fb15fa9abfaf20c0cc803e5bb6
DOHEE <dohee212@naver.com>
2022-08-20 16:13:21 +0900

    [Feat] loginRequired 추가

--- back/src/routers/followRouter.js
+++ back/src/routers/followRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { FollowService } from "../services/FollowService";
+import loginRequired from "../middlewares/loginRequired";
+
 const followRouter = Router();
 
-followRouter.post("/follow/:user_id", async (req, res, next) => {
+followRouter.post("/follow/:user_id", loginRequired, async (req, res, next) => {
     try {
         const userId = req.currentUserId;
         const kurlyencerId = req.params.user_id;
@@ -14,16 +16,20 @@ followRouter.post("/follow/:user_id", async (req, res, next) => {
     }
 });
 
-followRouter.delete("/unfollow/:user_id", async (req, res, next) => {
-    try {
-        const userId = req.currentUserId;
-        const kurlyencerId = req.params.user_id;
+followRouter.delete(
+    "/unfollow/:user_id",
+    loginRequired,
+    async (req, res, next) => {
+        try {
+            const userId = req.currentUserId;
+            const kurlyencerId = req.params.user_id;
 
-        const unfollow = "success";
-        res.status(200).json(unfollow);
-    } catch (error) {
-        next(error);
+            const unfollow = "success";
+            res.status(200).json(unfollow);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
 export { followRouter };
