3e0666ebabeab5b9bc930f9cb6fbd7049fbc8b3e
DOHEE <dohee212@naver.com>
2022-08-20 22:30:12 +0900

    [Feat] 좋아요 취소 라우터

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -18,4 +18,17 @@ likeRouter.post("/:review_id", loginRequired, async (req, res, next) => {
     }
 });
 
+likeRouter.delete("/:review_id", loginRequired, async (req, res, next) => {
+    try {
+        const userId = req.currentUserId;
+        const reviewId = req.params.review_id;
+
+        const like = "success";
+
+        res.status(200).json(like);
+    } catch (error) {
+        next(error);
+    }
+});
+
 export { likeRouter };
