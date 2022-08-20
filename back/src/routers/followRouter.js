6af7b96f84daefa3089166da83731dd32115d2f0
DOHEE <dohee212@naver.com>
2022-08-21 03:31:37 +0900

    [Feat] self follow 및 unfollow 제한

--- back/src/routers/followRouter.js
+++ back/src/routers/followRouter.js
@@ -9,6 +9,10 @@ followRouter.post("/:user_id", loginRequired, async (req, res, next) => {
         const userId = req.currentUserId;
         const kurlyencerId = req.params.user_id;
 
+        if (userId === kurlyencerId) {
+            throw new Error("스스로를 팔로우할 수 없습니다.");
+        }
+
         const follow = await FollowService.followUser({ userId, kurlyencerId });
         res.status(200).json(follow);
     } catch (error) {
@@ -20,6 +24,10 @@ followRouter.delete("/:user_id", loginRequired, async (req, res, next) => {
     try {
         const userId = req.currentUserId;
         const kurlyencerId = req.params.user_id;
+
+        if (userId === kurlyencerId) {
+            throw new Error("스스로를 언팔로우할 수 없습니다.");
+        }
 
         const unfollow = await FollowService.unfollowUser({
             userId,
