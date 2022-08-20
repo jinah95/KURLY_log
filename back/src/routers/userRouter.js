5af24e5c4807f9985d153cd0da10358c0494f69b
DOHEE <dohee212@naver.com>
2022-08-21 04:56:21 +0900

    [Feat] 사용자 프로필 수정 기능 라우터 초안

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { UserService } from "../services/UserService";
+import { loginRequired } from "../middlewares/loginRequired";
+import { User } from "../db/models/User";
 const userRouter = Router();
 
 userRouter.post("/login", async (req, res, next) => {
@@ -27,6 +29,26 @@ userRouter.get("/user/:user_id", async (req, res, next) => {
     try {
         const userId = req.params.user_id;
         const user = await UserService.getUserInfo(userId);
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+userRouter.patch("/user/:user_id", loginRequired, async (req, res, next) => {
+    try {
+        const loginId = req.currentUserId;
+        const userId = req.params.user_id;
+
+        if (loginId !== userId) {
+            throw new Error("수정 권한이 없는 사용자입니다.");
+        }
+
+        const nickname = req.body.nickname;
+        const updateData = { nickname };
+
+        const user = await UserService.updateProfile({ userId, updateData });
+
         res.status(200).json(user);
     } catch (error) {
         next(error);
