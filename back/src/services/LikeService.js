e4bf44624c43479270ede1a70291d0c7e419ac51
DOHEE <dohee212@naver.com>
2022-08-20 22:39:56 +0900

    [Feat] 좋아요 여부를 조회하는 기능 추가

--- back/src/services/LikeService.js
+++ back/src/services/LikeService.js
@@ -4,6 +4,12 @@ class LikeService {
     static async likeReview({ userId, reviewId }) {
         const like = { user_id: userId, review_id: reviewId };
 
+        const likeOrNot = await Like.findByFilter(like);
+
+        if (likeOrNot) {
+            throw new Error("이미 좋아요를 누른 게시글입니다.");
+        }
+
         const newLike = "like";
 
         return newLike;
@@ -11,6 +17,14 @@ class LikeService {
 
     static async unlikeReview({ userId, reviewId }) {
         const like = { user_id: userId, review_id: reviewId };
+
+        const likeOrNot = await Like.findByFilter(like);
+
+        if (!likeOrNot) {
+            throw new Error(
+                "좋아요를 누르지 않았거나 이미 좋아요를 취소한 게시글입니다."
+            );
+        }
 
         const newUnlike = "unlike";
 
