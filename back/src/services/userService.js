6aac5b89d96518dd3492f2e47e2202845fd02f7c
DOHEE <dohee212@naver.com>
2022-08-21 03:55:15 +0900

    [Feat] 유저 정보 조회 기능의 에러메시지 형식 일부 수정

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -3,53 +3,54 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 class UserService {
-  static async getUsers() {
-    const users = await User.findAll();
-    if (!users) {
-      const errorMessage = "유저가 없습니다.";
-      throw new Error(errorMessage);
-    }
-    return users;
-  }
-
-  static async getUserInfo(userId) {
-    const user = await User.findById(userId);
-    if (!user) {
-      const errorMessage = "해당 유저가 없습니다.";
-      throw new Error(errorMessage);
-    }
-    return user;
-  }
-
-  static async getUser({ nickname, password }) {
-    const user = await User.findByNickname(nickname);
-    if (!user) {
-      const errorMessage = "해당 유저가 없습니다.";
-      throw new Error(errorMessage);
-    }
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordCorrect) {
-      const errorMessage = "잘못입력하셨습니다.";
-      throw new Error(errorMessage);
+    static async getUsers() {
+        const users = await User.findAll();
+        if (!users) {
+            const errorMessage = "유저가 없습니다.";
+            throw new Error(errorMessage);
+        }
+        return users;
     }
 
-    // last login update 추가
+    static async getUserInfo(userId) {
+        const user = await User.findById(userId);
 
-    const secretKey = process.env.JWT_SECRET_KEY;
-    const token = jwt.sign({ userId: user.user_id }, secretKey);
+        if (!user) {
+            throw new Error("해당 유저가 없습니다.");
+        }
 
-    const { user_id, picture, grade, intro } = user;
-    const loginUser = {
-      token,
-      userId: user_id,
-      nickname,
-      picture,
-      grade,
-      intro,
-    };
-    return { message: "success", data: loginUser };
-  }
+        return user;
+    }
+
+    static async getUser({ nickname, password }) {
+        const user = await User.findByNickname(nickname);
+        if (!user) {
+            const errorMessage = "해당 유저가 없습니다.";
+            throw new Error(errorMessage);
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            const errorMessage = "잘못입력하셨습니다.";
+            throw new Error(errorMessage);
+        }
+
+        // last login update 추가
+
+        const secretKey = process.env.JWT_SECRET_KEY;
+        const token = jwt.sign({ userId: user.user_id }, secretKey);
+
+        const { user_id, picture, grade, intro } = user;
+        const loginUser = {
+            token,
+            userId: user_id,
+            nickname,
+            picture,
+            grade,
+            intro,
+        };
+        return { message: "success", data: loginUser };
+    }
 }
 
 export { UserService };
