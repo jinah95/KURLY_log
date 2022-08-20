c7b9371244a26519e26b4c7682cee3659401bba6
DOHEE <dohee212@naver.com>
2022-08-20 22:36:57 +0900

    [Feat] filter를 통해 좋아요 조회 기능 추가

--- back/src/db/models/Like.js
+++ back/src/db/models/Like.js
@@ -13,6 +13,12 @@ const Like = {
         return newLike;
     },
 
+    findByFilter: async (filter) => {
+        const likeOrNot = await likeModel.findOne({ where: filter });
+
+        return likeOrNot;
+    },
+
     delete: async ({ like }) => {
         const newUnlike = await likeModel.destroy(like);
 
