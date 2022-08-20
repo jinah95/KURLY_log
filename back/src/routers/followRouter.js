aef87fcf9f101576aded0cfe3916d0195ceca4b3
DOHEE <dohee212@naver.com>
2022-08-20 15:41:05 +0900

    [Feat] follow 기능 초안

--- back/src/routers/followRouter.js
+++ back/src/routers/followRouter.js
@@ -2,4 +2,16 @@ import { Router } from "express";
 // import { FollowService } from "../services/FollowService";
 const followRouter = Router();
 
+followRouter.post("/follow/:user_id", async (req, res, next) => {
+    try {
+        const userId = req.currentUserId;
+        const kurlyencerId = req.params.user_id;
+
+        const follow = "success";
+        res.status(200).json(follow);
+    } catch (error) {
+        next(error);
+    }
+});
+
 export { followRouter };
