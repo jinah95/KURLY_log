d2667b5bc3c0e07915cf31b781aa92813ce34756
DOHEE <dohee212@naver.com>
2022-08-20 15:57:45 +0900

    [Fix] 오타 수정

--- back/src/services/FollowService.js
+++ back/src/services/FollowService.js
@@ -2,7 +2,7 @@
 
 class FollowService {
     static async followUser({ userId, kurlyencerId }) {
-        const newFollow = { user_id: userId, followerId: kurlyencerId };
+        const newFollow = { user_id: userId, follower_id: kurlyencerId };
         const createdNewFollow = newFollow;
 
         return createdNewFollow;
