dc799fb2b1455f067c5c8d544309dabdd7119676
DOHEE <dohee212@naver.com>
2022-08-22 15:48:58 +0900

    [Fix] 저장 미스 해결

--- back/src/middlewares/loginRequired.js
+++ back/src/middlewares/loginRequired.js
@@ -1,28 +1,14 @@
 import jwt from "jsonwebtoken";
-const { development, JWT_SECRET_KEY } = process.env;
 
 const loginRequired = (req, res, next) => {
-  // request 헤더로부터 authorization bearer 토큰을 받음.
   const userToken = req.headers["authorization"]?.split(" ")[1] ?? null;
-
-  // 이 토큰은 jwt 토큰 문자열이거나, 혹은 "null" 문자열임.
-  // 토큰이 "null" 일 경우, loginRequired 가 필요한 서비스 사용을 제한함.
   if (!userToken) {
-    if (!development) {
-      console.log(
-        "서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음"
-      );
-    }
-    res.status(400).json("로그인한 유저만 사용할 수 있는 서비스입니다.");
+    res.status(400).json("로그인 해주세요.");
     return;
   }
-
-  // 해당 token 이 정상적인 token인지 확인 -> 토큰에 담긴 user_id 정보 추출
   try {
-    if (!JWT_SECRET_KEY) {
-      throw new Error("JWT_SECRET_KEY가 존재하지 않습니다.");
-    }
-    const jwtDecoded = jwt.verify(userToken, JWT_SECRET_KEY);
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const jwtDecoded = jwt.verify(userToken, secretKey);
     const userId = jwtDecoded.userId;
     req.currentUserId = userId;
     next();
