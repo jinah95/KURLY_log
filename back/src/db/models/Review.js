cc13730899b11de5a6717b4d2402f72afb8bd1e3
guswn2521@gmail.com <guswn2521@gmail.com>
2022-08-22 20:06:45 +0900

    chore: 컬리로그 1개 조회에 좋아요수 축가

--- back/src/db/models/Review.js
+++ back/src/db/models/Review.js
@@ -193,16 +193,31 @@ const Review = {
   getLog: async ({ reviewId }) => {
     console.log(reviewId);
     const log = await reviewModel.findOne({
+      attributes: {
+        include: [
+          [
+            sequelize.fn("COUNT", sequelize.col("like.review_id")),
+            "likesCount",
+          ],
+        ],
+      },
       include: [
         {
           model: userModel,
           as: "user",
           attributes: { exclude: ["password", "register_date", "last_login"] },
         },
+        {
+          model: likeModel,
+          as: "like",
+          attributes: [],
+          required: false,
+        },
       ],
       where: {
         review_id: reviewId,
       },
+      group: ["reviews.review_id", "user.user_id"],
     });
     return log;
   },
