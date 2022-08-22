b0679a86a03c583f7532595cd77a49cd70acb343
DOHEE <dohee212@naver.com>
2022-08-22 19:35:45 +0900

    [Feat] 개별 상품 조회 서비스 초안

--- back/src/services/ProductService.js
+++ back/src/services/ProductService.js
@@ -5,6 +5,11 @@ const ProductService = {
     const products = await Product.findAll();
     return { message: "success", data: products };
   },
+
+  getProduct: async ({ productId }) => {
+    const product = await Product.findById(productId);
+    return { message: "success", data: product };
+  },
 };
 
 export { ProductService };
