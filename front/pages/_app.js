da762e3119e0a7d061021bb21eaff518a33a8821
jinah park <tbrk8585@naver.com>
2022-08-23 11:35:57 +0900

    feat: stay login , current user api add

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useReducer, createContext } from "react";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { loginReducer } from "../provider/reducer";
+import { get } from "../api";
 
 export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
@@ -10,7 +11,40 @@ function MyApp({ Component, pageProps }) {
     const [userState, dispatch] = useReducer(loginReducer, {
         user: null,
     });
+    const [isFetchCompleted, setIsFetchCompleted] = useState(false);
 
+    const fetchCurrentUser = async () => {
+        try {
+            const currentUser = sessionStorage.getItem("userToken");
+
+            if (currentUser) {
+                const res = await get("/users/current");
+                const user = res.data.data;
+
+                dispatch({
+                    type: "LOGIN_SUCCESS",
+                    payload: user,
+                });
+            } else {
+                console.error(
+                    "%c SessionStorage에 토큰 없음.",
+                    "color: #d93d1a;"
+                );
+            }
+        } catch (err) {
+            console.error("error message: ", err);
+        }
+
+        setIsFetchCompleted(true);
+    };
+
+    useEffect(() => {
+        fetchCurrentUser();
+    }, []);
+
+    if (!isFetchCompleted) {
+        return "loading...";
+    }
     return (
         <DispatchContext.Provider value={dispatch}>
             <UserStateContext.Provider value={userState}>
