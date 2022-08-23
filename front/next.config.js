53438dfe1bc6de6acf5e62039670b9a3645c5cea
aldrn <aldrn29@gmail.com>
2022-08-23 19:31:53 +0900

    chore: s3 도메인 추가

--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["images.dog.ceo", "images.unsplash.com"],
+        domains: ["images.dog.ceo", "images.unsplash.com", "ssap-kurly.s3.ap-northeast-2.amazonaws.com"],
     },
     swcMinify: true,
 };
