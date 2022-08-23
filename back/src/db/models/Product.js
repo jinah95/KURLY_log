0d83d17fa0fd72bfa51ba92ac0e80f198759e8f2
guswn2521@gmail.com <guswn2521@gmail.com>
2022-08-24 02:26:09 +0900

    chore: 상품전체조회 리뷰수순

--- back/src/db/models/Product.js
+++ back/src/db/models/Product.js
@@ -16,8 +16,14 @@ const Product = {
   },
 
   findAll: async () => {
-    const products = await productModel.findAll({});
-    return products;
+    const products = await sequelize.query(`
+    select p.product_id, p.detail, p.price, p.image, cnt from products p
+    join (select product_id , count(review_id) as cnt
+    from reviews r group by product_id ) c
+    on p.product_id = c.product_id
+    order by cnt desc
+    `);
+    return products[0];
   },
 };
 
