81db59ade5f77e9ebecb14880dc2b790fd5a8a3e
aldrn <aldrn29@gmail.com>
2022-08-24 13:58:23 +0900

    fix: 글쓰기 기능 도구 정리

--- front/components/KurlyLogWrite.js
+++ front/components/KurlyLogWrite.js
@@ -298,10 +298,19 @@ const Wrapper = styled.div`
 
 const ViewContainer = styled.div`
     border: 1px solid #e2e2e2;;
-    margin: 10px;
+    padding: 10px;
+    min-height: 300px;
 
     .ql-align-center {
         text-align: center;
+    }
+
+    .ql-align-left {
+        text-align: left;
+    }
+
+    .ql-align-right {
+        text-align: right;
     }
 
     .ql-syntax {
