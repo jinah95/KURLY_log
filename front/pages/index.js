ff8c28898872b6ad9d31eb411d1b81976076a3c1
jinah park <tbrk8585@naver.com>
2022-08-30 14:28:23 +0900

    feat: productlist ssr add

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,15 @@
 import Best from "../components/Best";
+import { getPost } from "../api";
 
-export default function Home() {
-    return <Best />;
+export const getServerSideProps = async (context) => {
+    const res = await getPost("/goods");
+    const newArr = res.data.data || null;
+
+    return {
+        props: { newArr },
+    };
+};
+
+export default function Home({ newArr }) {
+    return <Best newArr={newArr} />;
 }
